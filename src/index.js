edf5e2b0615247da013692b911d9a137aab0239b
yuvalzoref <yuval_zoref@hotmail.com>
2023-08-16 16:59:16 +0100

    about me page almost finished with javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello Webpack Project.');
+// const tablinks = document.getElementsByClassName("tab-links");
+// const tabcontents = document.getElementsByClassName("tab-contents");
+
+// function opentab(tabName){
+//   for(const tablink of tablinks){
+//     tablink.classList.remove("active-link");
+//   }
+//   for(const tabcontent of tabcontents){
+//     tabcontent.classList.remove("active-tab");
+//   }
+//   event.currentTarget.classList.add("active-link");
+//   document.getElementById(tabName).classList.add("active-tab");
+// }
